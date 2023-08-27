2408279c6a7d172df77dafb35952825147fbf970
Matt Payne <Payne@MattPayne.org>
2023-08-27 07:32:53 -0500

    Switch back to a {} and away from `new Map`

--- typescript/grade-school/grade-school.ts
+++ typescript/grade-school/grade-school.ts
@@ -1,6 +1,6 @@
 export class GradeSchool {
-  // people:any = {} // grade to list of people
-  people:Map<number, string[]> = new Map<number, string[]>();
+  people:any = {} // grade to list of people
+  // people:Map<number, string[]> = new Map<number, string[]>();
 
   roster() {
     return this.people
@@ -12,6 +12,19 @@ export class GradeSchool {
   }
 
   add(name:string, classGrade:number) {
+    let inGrade = this.people[classGrade]
+    if (!inGrade) {
+      inGrade = []
+    }
+    inGrade.push(name)
+    this.people[classGrade] = inGrade
+  }
+
+  grade(classGrade:number): string[] {
+    return this.people[classGrade]
+  }
+
+  addMap(name:string, classGrade:number) {
     let inGrade = this.people.get(classGrade)
     if (!inGrade) {
       inGrade = []
@@ -20,7 +33,7 @@ export class GradeSchool {
     this.people.set(classGrade, inGrade)
   }
 
-  grade(classGrade:number): string[] {
+  gradeMap(classGrade:number): string[] {
     return this.people.get(classGrade) as string[];
   }
 }
