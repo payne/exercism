e817bc51cd3ba536878350053e4be827766ddd80
Matt Payne <Payne@MattPayne.org>
2023-08-27 21:17:52 -0500

    wip - stuck on this test failing.  SHM UGH.

--- typescript/grade-school/grade-school.ts
+++ typescript/grade-school/grade-school.ts
@@ -12,12 +12,31 @@ export class GradeSchool {
   }
 
   add(name:string, classGrade:number) {
+    this.remove(name)
     let inGrade = this.people[classGrade]
-    if (!inGrade) {
-      inGrade = []
-    }
+    inGrade = inGrade || []
+    if (classGrade == 1 && name === 'Aimee') console.log('Asking about Amiee: ', inGrade)
     inGrade.push(name)
     this.people[classGrade] = this.sort(inGrade)
+    if (classGrade == 1 && name === 'Aimee') console.log('after storing and sorting: Asking about Amiee: ', inGrade)
+  }
+
+  remove(name: string) {
+    console.log(`start of remove`, JSON.stringify(this.people))
+    for (let gradeLevel in this.people) {
+      let students = this.people[gradeLevel]
+      const oldLen = students.length
+      students = students.filter((s:string) => s !== name)
+      if (students === undefined) { students = [] }
+      this.people[gradeLevel] = students
+      if (oldLen != students.length) { 
+        console.log(`students.length changed! students=`, students) 
+        console.log(JSON.stringify(this.people))
+        const grade2  = this.grade(2)
+        console.log(`grade2`, grade2)
+      }
+    }
+    console.log(`end of remove`, JSON.stringify(this.people))
   }
 
   sort(names: string[]): string[] {
@@ -27,10 +46,12 @@ export class GradeSchool {
 
   grade(classGrade:number): string[] {
     let names = this.people[classGrade]
-    if (names) {
-      names = this.sort(names)
-    } else { names = [] }
-    return names
+    names = names || []
+    names = this.sort(names)
+    let clone = JSON.parse(JSON.stringify(names))
+    clone = clone || []
+    if (classGrade === 2) { console.log(`clone is`, clone); }
+    return clone.freeze
   }
 
 }
